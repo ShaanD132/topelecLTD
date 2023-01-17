b58dc226bb74c8bb1b12e722690dc2e1d46928b5
Shaan <shaanduss@gmail.com>
2023-01-17 11:21:07 +0800

    image fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       <div className = "main flex flex-col">
         <Navbar />
-        <img alt = "industrial" src = "/assets/test.png" className = "absolute z-1 main-image w-full h-full object-cover"/>
+        <Image width = {1000} height = {1000} alt = "industrial" src = "/assets/test.png" className = "absolute z-1 main-image w-full h-full object-cover"/>
         <MainText />
         <div className = "relative flex-grow-1 z-10 items-center flex justify-center text-center mb-4">
           <Image width = {100} height = {100} alt = "scroll-down" src = "/assets/arrow.png"/>
