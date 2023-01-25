b226b6ae16f57c474fcce60918e28b6e0f804d77
Shaan <shaanduss@gmail.com>
2023-01-25 11:03:01 +0800

    please

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Topelec</title>
+        <title>Topelec LTD</title>
         <meta name="description" content="Topelec LTD - Mauritius" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
