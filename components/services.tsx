e19ca5d6c9804b54b0e8efa8a3f414d925521c77
Shaan <shaanduss@gmail.com>
2023-01-25 11:00:08 +0800

    trial

--- components/services.tsx
+++ components/services.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const Services: React.FC = () => (
     <div className = "services flex flex-col items-center justify-center" id = "services">
-        <p className = "mt-32 font-hb text-3xl">Services</p>
+        <p className = "mt-32 font-hb text-3xl">Our Services</p>
     </div>
 )
-
 
 
 
