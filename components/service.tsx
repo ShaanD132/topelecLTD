e297d10dcea2fbd5c739edcb6c15ced26eb54f73
Shaan <shaanduss@gmail.com>
2023-01-26 15:53:04 +0800

    hover changes

--- components/service.tsx
+++ components/service.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const Service: React.FC<Props> = ({id, text}) => (
-    <div className = "px-10 product">
+    <div className = "px-10 service">
         <Image width = {350} height = {350} alt = {text} src = {`/assets/services/${id}.png`} className = "overflow-hidden rounded-lg" />
     </div>
 )
