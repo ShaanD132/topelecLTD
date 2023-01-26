d5b97209d584a8957b2c7007dde446000d20908c
Shaan <shaanduss@gmail.com>
2023-01-26 16:21:33 +0800

    logo changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Link from "next/link"
+import Image from "next/image"
 
 const Navbar: React.FC = () => {
     return(
         <div className = "navbar drop-shadow-[0_4px_4px_rgba(0,0,0,0.05)] fixed w-full z-10">
             <div className = "flex justify-between flex-row mx-20 py-5">
-                <a href = "#home"><h1 className = "text-3xl font-mb main-logo">TOPELEC LTD</h1></a>
+                <a href = "#home">
+                    <div className = "flex flex-row items-center justify-center main-logo">
+                    <Image width = {50} height = {50} alt = "logo" src = "/assets/black_logo.png" />
+                    <h1 className = " ml-3 text-3xl font-mb">TOPELEC LTD</h1>
+                    </div></a>
                 <div className = "flex flex-row text-xl mt-2 font-h">
                     <Link href = "/" className = "hover:text-blu"><h2 className = "mx-6">Home</h2></Link>
                     <Link href = "#products" className = "hover:text-blu"><p className = "mx-6">Products</p></Link>
