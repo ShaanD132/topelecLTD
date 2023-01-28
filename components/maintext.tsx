a1d3830ed97e68fd4760ce34b03e729e0daa114d
Shaan <shaanduss@gmail.com>
2023-01-28 15:05:46 +0800

    responsive

--- components/maintext.tsx
+++ components/maintext.tsx
@@ -4,8 +4,8 @@ const MainText: React.FC = () => {
     return(
         <div className = "relative z-10 flex-1 flex items-left justify-center flex-col px-20 mt-28" >
             <div>
-                <h1 className = "font-bold text-5xl line font-hb">QUALITY <br/> MADE <span className = "text-main-yellow">AFFORDABLE</span></h1>
-                <div className = "text-2xl mt-2 font-o">
+                <h1 className = "font-bold text-3xl md:text-4xl lg:text-5xl line font-hb">QUALITY <br/> MADE <span className = "text-main-yellow">AFFORDABLE</span></h1>
+                <div className = "text-lg md:text-xl lg:text-2xl mt-2 font-o">
                     <p>Topelec Ltd, founded in 2002, is an importer, wholesaler, & retailer of: </p>
                     <ul className = "px-12 py-3">
                         <li>electrical cabling</li>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,17 +5,17 @@ import Image from "next/image"
 const Navbar: React.FC = () => {
     return(
         <div className = "navbar drop-shadow-[0_4px_4px_rgba(0,0,0,0.05)] fixed w-full z-10">
-            <div className = "flex justify-between flex-row mx-20 py-5">
+            <div className = "flex justify-between flex-row mx-8 md:mx-20 py-5">
                 <Link href = "/">
                     <div className = "flex flex-row items-center justify-center main-logo">
-                    <Image width = {50} height = {50} alt = "logo" src = "/assets/black_logo.png" />
-                    <h1 className = " ml-3 text-3xl font-mb">TOPELEC LTD</h1>
+                    <Image width = {50} height = {50} alt = "logo" src = "/assets/black_logo.png" className = "hidden md:block"/>
+                    <h1 className = "sm:ml-0 md:ml-3 md:text-2xl xl:text-3xl font-mb">TOPELEC LTD</h1>
                     </div></Link>
-                <div className = "flex flex-row text-xl mt-2 font-h">
-                    <Link href = "/" className = "hover:text-blu"><h2 className = "mx-6">Home</h2></Link>
-                    <Link href = "#products" className = "hover:text-blu"><p className = "mx-6">Products</p></Link>
-                    <Link href = "#services" className = "hover:text-blu"><p className = "mx-6 hover:text-blu">Services</p></Link>
-                    <Link href = "#about" className = "hover:text-blu"><p className = "mx-6 hover:text-blu">About Us</p></Link>
+                <div className = "flex flex-row md:text-lg xl:text-xl md:mt-2 font-h">
+                    <Link href = "/" className = "hover:text-blu"><h2 className = "mx-2 md:mx-6">Home</h2></Link>
+                    <Link href = "#products" className = "hover:text-blu"><p className = "mx-2 md:mx-6">Products</p></Link>
+                    <Link href = "#services" className = "hover:text-blu"><p className = "mx-2 md:mx-6 hover:text-blu">Services</p></Link>
+                    <Link href = "#about" className = "hover:text-blu"><p className = "mx-2 md:mx-6 hover:text-blu">About Us</p></Link>
                 </div>
             </div>
         </div>
