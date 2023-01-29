f0e23e5a34b7bbda760cd47e06dc6f1849c16bba
Shaan <shaanduss@gmail.com>
2023-01-29 16:48:34 +0800

    socials

--- components/maintext.tsx
+++ components/maintext.tsx
@@ -2,17 +2,11 @@ import React from "react";
 
 const MainText: React.FC = () => {
     return(
-        <div className = "relative z-10 flex-1 flex items-left justify-center flex-col px-20 mt-10 md:mt-28" >
+        <div className = "relative z-10 flex-1 flex items-left justify-center flex-col px-20" >
             <div>
                 <h1 className = "text-3xl md:text-5xl lg:text-5xl line font-hb">QUALITY <br/> MADE <span className = "text-main-yellow">AFFORDABLE</span></h1>
-                <div className = "text-lg md:text-2xl lg:text-2xl mt-2 font-o">
-                    <p>Topelec Ltd, founded in 2002, is an importer, wholesaler, & retailer of: </p>
-                    <ul className = "px-12 py-3">
-                        <li>electrical cabling</li>
-                        <li>switch gears</li>
-                        <li>protection devices</li>
-                        <li>other electrical, mechanical, automation <br />and data accessories.</li>
-                    </ul>
+                <div className = "text-lg md:text-2xl lg:text-2xl mt-2 font-o w-1/2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet ipsum consequat, eleifend est eget, rhoncus velit.</p>
                 </div>
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import MainText from '@/components/maintext'
 import Values from "@/components/values"
 import Products from "@/components/products"
 import Services from "@/components/services"
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -15,6 +16,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className = "fixed z-30 bottom-10 right-10 hidden md:block">
+        <div className = "flex flex-row bg-white rounded-lg drop-shadow-[0_3px_5px_rgba(0,0,0,0.2)]">
+          <Link href = "https://www.instagram.com/topelecltd/"><Image width = {30} height = {30} alt = "instagram" src = "/assets/logos/instagram.png" className = "mx-4 my-2 hover:text-blue"/></Link>
+          <Link href = "https://www.instagram.com/topelecltd/"><Image width = {30} height = {30} alt = "email" src = "/assets/logos/mail.png" className = "mx-4 my-2 hover:opacity-96"/></Link>
+          <Link href = "https://www.instagram.com/topelecltd/"><Image width = {30} height = {30} alt = "facebook" src = "/assets/logos/facebook.png" className = "mx-4 my-2"/></Link>
+        </div>
+      </div>
 
       <div className = "main flex flex-col bg-white md:bg-black" id = "home">
         <Image width = {1000} height = {1000} alt = "industrial" src = "/assets/industrial.jpeg" className = "absolute hidden md:block z-1 h-full w-full object-cover"/>
