cbd3a80c4722aec0ae5540a0a84fdc0a077233fd
Shaan <shaanduss@gmail.com>
2023-01-29 16:22:41 +0800

    margin

--- components/maintext.tsx
+++ components/maintext.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainText: React.FC = () => {
     return(
-        <div className = "relative z-10 flex-1 flex items-left justify-center flex-col px-20 mt-28" >
+        <div className = "relative z-10 flex-1 flex items-left justify-center flex-col px-20 mt-10 md:mt-28" >
             <div>
                 <h1 className = "text-3xl md:text-5xl lg:text-5xl line font-hb">QUALITY <br/> MADE <span className = "text-main-yellow">AFFORDABLE</span></h1>
                 <div className = "text-lg md:text-2xl lg:text-2xl mt-2 font-o">
