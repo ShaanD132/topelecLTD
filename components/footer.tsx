9481986a8be2495661af34ffd4b3eb77bf19ae35
Shaan <shaanduss@gmail.com>
2023-11-07 15:36:56 +0800

    footer fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,13 +7,16 @@ const Footer: React.FC = () => (
             <div className = "mt-10">
                 <h1 className = "text-2xl font-ob text-center">Topelec LTD</h1>
                 <div className = "justify-between font-o mt-5 text-xl px-20 lg:px-72 lg:flex">
-                    <Link href="/contact" className="hover:underline"><p className = "mt-1">About Us</p></Link>
+                    <p className = "mt-1 hover:underline">About Us</p>
                     <Link href="/contact" className="hover:underline"><p className = "mt-1">Our Projects</p></Link>
                     <Link href="/contact" className="hover:underline"><p className = "mt-1">Contact Us</p></Link>
                 </div>
+                <div className = "x-20 lg:px-72 mt-5 font-lg">
+                    <p className = "font-o"> CNR Praslin & Reserves ST,<br/> Les Salines Port-Louis, <br/>Mauritius.</p>
+                </div>
                 <div className = "mt-10">
                     <hr/>
-                    <h1 className = " my-2 text-md text-center font-at">developed by <Link target="_blank" href="https://shaanduss.vercel.app" className = "hover:underline">shaan dussoye</Link></h1>
+                    <h1 className = " my-2 text-md text-center font-at">developed by <Link target="_blank" href="https://shaanduss.vercel.app" className = "hover:under">shaan dussoye</Link></h1>
                 </div>
             </div>
     </div>
